74e68be05f94a160b393e6d6279dad0263e5990c
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-01-25 11:06:11 +0900

    Fix for titles do not have id

--- docs/js/formatter/title.js
+++ docs/js/formatter/title.js
@@ -1,7 +1,13 @@
 const titleRe = /\.\s+/;
 export function formatTitle(head) {
   const [id, title] = head.split(titleRe);
-  const lv = id.split('.').length + 1;
 
-  return `${'#'.repeat(lv)} ${id}. ${title}`;
+  // e.g.: 1. Introduction
+  if (id && title) {
+    const lv = id.split('.').length + 1;
+    return `${'#'.repeat(lv)} ${id}. ${title}`;
+  }
+
+  // e.g.: Abstract
+  return `${'#'.repeat(2)} ${head}`;
 }
